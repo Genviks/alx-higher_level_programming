25bb82c934b63e7ddd80a17e0b85aebae261cabd
Genevieve Onwuka <genevievechigoziri@gmail.com>
2023-11-02 17:12:54 -0700

    Write a program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    import sys
     from calculator_1 import add, sub, mul, div
-    argc = len(argv)
-    if argc != 4:
-        print('usage: {} <a> <operator> <b>'.format(argv[0]))
-        exit(1)
-    ops = {
+
+
+if len(sys.argv) != 4:
+    print("usage: {} <a> <operator> <b>".format(sys.argv[0]))
+    sys.exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+
+operators = {
         '+': add,
         '-': sub,
         '*': mul,
         '/': div,
     }
-    if argv[2] in ops:
-        num1 = int(argv[1])
-        num2 = int(argv[3])
-        op = ops[argv[2]]
-        result = op(num1, num2)
-        print('{:d} {:s} {:d} = {:d}'.format(num1, argv[2], num2, result))
-    else:
-        print('Unknown operator, Available operators: +, -, *, and /')
-        exit(1)
-    exit(0)
+if operator in operators:
+    result = operators[operator](a, b)
+    print('{} {} {} = {}'.format(a, operator, b, result))
+else:
+    print('Unknown operator. Available operators: +, -, *, and /')
+    sys.exit(1)
